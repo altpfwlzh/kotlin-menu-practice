a961029601543a83b9a1fbb7d62d9b77d6933830
altpfwlzh <altpfwlzh@gmail.com>
2023-12-08 15:05:30 +0900

    feat(view/OutputView) : OutputView 출력 정리

--- src/main/kotlin/menu/constants/Strings.kt
+++ src/main/kotlin/menu/constants/Strings.kt
@@ -1,5 +1,12 @@
 package menu.constants
 
 object Strings {
-
+    const val OUTPUT_HELLO = "점심 메뉴 추천을 시작합니다."
+    const val OUTPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)"
+    const val OUTPUT_COACH_INVALID_MENU = "%s가 못 먹는 메뉴를 입력해 주세요."
+    const val OUTPUT_RECOMMENDED_MENUS = "메뉴 추천 결과입니다."
+    const val OUTPUT_RECOMMENDED_DAYS = "[ 구분 | %s | %s | %s | %s | %s ]"
+    const val OUTPUT_RECOMMENDED_CATEGORIES = "[ 카테고리 | %s | %s | %s | %s | %s ]"
+    const val OUTPUT_COACH_RECOMMENDED_MENUS = "[ %s | %s | %s | %s | %s | %s ]"
+    const val OUTPUT_BYE = "추천을 완료했습니다."
 }
--- src/main/kotlin/menu/view/OutputView.kt
+++ src/main/kotlin/menu/view/OutputView.kt
@@ -6,4 +6,23 @@ import menu.constants.Strings
 class OutputView() {
     private val strings: Strings = Strings
     private val errorMessage: ErrorMessage = ErrorMessage
+
+    fun printHello() = println(strings.OUTPUT_HELLO)
+
+    fun printCoachesName() = println(strings.OUTPUT_COACHES_NAME)
+
+    fun printCoachInvalidMenu() = println(strings.OUTPUT_COACH_INVALID_MENU)
+
+    fun printRecommendedMenu() = println(strings.OUTPUT_RECOMMENDED_MENUS)
+
+    fun printRecommendedDays() = println(strings.OUTPUT_RECOMMENDED_DAYS)
+
+    fun printRecommendedCategories() = println(strings.OUTPUT_RECOMMENDED_CATEGORIES)
+
+    fun printCoachRecommendedMenus() = println(strings.OUTPUT_COACH_RECOMMENDED_MENUS)
+
+    fun printBye() = println(strings.OUTPUT_BYE)
+
+    private fun printBlankLine() = println()
+
 }
